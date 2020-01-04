e8e17920ed76d13b8d59e878322580c0532309e0
15116 <15116952620@163.com>
2020-01-04 16:09:36 +0800

    freemarker 带参返回

---
+++ src/test/java/org/cn/test/Test2.java
@@ -0,0 +1,36 @@
+package org.cn.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Test2 {
+
+    @Test
+    public void test1(){
+
+//        freemarker 参数返回
+//        @Autowired
+//        protected HttpServletRequest request;
+//
+//        public UserController setAttr(String name, Object value) {
+//            request.setAttribute(name, value);
+//            return this;
+//        }
+//
+//        @RequestMapping("user/{id}")
+//        public String getUser(@PathVariable("id") String id){
+//            Map map = new HashMap<>();
+//            System.out.println(HtmlUtils.htmlEscape(id,"UTF-8"));
+//            setAttr("name", HtmlUtils.htmlEscape(id,"UTF-8"));
+//            setAttr("dd", "打分");
+//            return "login";
+//        }
+    }
+
+
+
+}
